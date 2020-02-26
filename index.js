317fa022e33943609e44206f301da5635272c9c8
DubstepKnight <nursmaster@gmail.com>
2020-02-26 20:33:04 +0200

    commented cors

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 
 // app.use(auth);
 
 app.use(bodyParser.json());
-app.use(cors);
+// app.use(cors);
 
 const usersRoute = require("./routes/v1/users");
 const postingsRoute = require("./routes/v1/postings");
