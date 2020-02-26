74c9cb567d7ed1e44392c275455ff5fd471ba00a
DubstepKnight <nursmaster@gmail.com>
2020-02-26 20:33:50 +0200

    changed the port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5001;
 
 // app.use(auth);
 
